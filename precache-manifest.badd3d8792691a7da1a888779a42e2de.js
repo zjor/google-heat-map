58b4d1f713ca59672ba1dab875430c291eaa488a
Sergey Royz <zjor.se@gmail.com>
2018-12-14 15:46:12 +0100

    Updates

---
+++ precache-manifest.badd3d8792691a7da1a888779a42e2de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/google-heat-map/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a4626c7ef52da8bc8a3a",
+    "url": "/google-heat-map/static/js/runtime~main.a4626c7e.js"
+  },
+  {
+    "revision": "bae3948aea1e233724d3",
+    "url": "/google-heat-map/static/js/main.bae3948a.chunk.js"
+  },
+  {
+    "revision": "335f833b2dbcb5022c09",
+    "url": "/google-heat-map/static/js/1.335f833b.chunk.js"
+  },
+  {
+    "revision": "bae3948aea1e233724d3",
+    "url": "/google-heat-map/static/css/main.1293cc86.chunk.css"
+  },
+  {
+    "revision": "d1f69001509cacba5780b19372b8931d",
+    "url": "/google-heat-map/index.html"
+  }
+];
